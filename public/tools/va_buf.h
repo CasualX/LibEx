5a42756bcc2ce421970ba1bb93c589e4ec037dc3
CursedGhost <empty>
2013-08-19 23:58:57 +0200

    Added va_printf function for convenient type deduction Added warning when used as a variable arg Fixed the buffer not being zero-terminated on overflow Fixed some bugs

--- public/tools/va_buf.h
+++ public/tools/va_buf.h
@@ -3,22 +3,37 @@
 #pragma once
 
 #include <cstdarg>
+#include <cassert>
 
 namespace tools
 {
 
+// WARNING!
+//
+// Do not pass this class directly to a function as a variable arg!
+// Instead, make sure to take the address before passing it.
+// It will work as expected (and auto-cast to char* as required) everywhere else.
+//
+// Example, will fail:
+// printf( "%s\n", va_printf<64>( "Hello %s!", "World" );
+// Do this instead:
+// printf( "%s\n", &va_printf<64>( "Hello %s!", "World" );
+//
+
+//------------------------------------------------
+// Class va_buf
+//------------------------------------------------
 // Create temporary buffers with formatted data
-template< unsigned long L, typename T = char >
+template< unsigned long L, typename T >
 class va_buf
 {
 	typedef typename T buffer_t[L];
 	typedef typename const T const_buffer_t[L];
 	buffer_t buf;
 public:
-	// Empty constructor, init buffer to empty string
+	// Empty constructor
 	inline va_buf()
 	{
-		((int*)buf)[0] = 0;
 	}
 	// Formatting constructor
 	inline va_buf( const T* fmt, ... )
@@ -26,15 +41,13 @@ public:
 		va_list va;
 		va_start( va, fmt );
 		printex( fmt, va );
-		va_end( va );	
 	}
 	// Formatting function
 	inline int print( const T* fmt, ... )
 	{
 		va_list va;
 		va_start( va, fmt );
-		printex( fmt, va );
-		va_end( va );	
+		return printex( fmt, va );
 	}
 
 	// Need different functions depending on character type, can't I directly specialize based on the provided class template?
@@ -42,22 +55,40 @@ public:
 	// Char version needs vsnprintf
 	template<> inline int printex<char>( const char* fmt, va_list va )
 	{
-		return ::vsnprintf( buf, sizeof(buf), fmt, va );
+		int n = ::vsnprintf( buf, L-1, fmt, va );
+		buf[n] = 0;
+		return n;
 	}
 #ifdef _MSC_VER
-	// Wide version needs _vsnwprintf (Windows only!)
+	// Wide version needs _vsnwprintf
 	template<> inline int printex<wchar_t>( const wchar_t* fmt, va_list va )
 	{
-		return ::_vsnwprintf( buf, sizeof(buf), fmt, va );
+		int n = ::_vsnwprintf( buf, L-1, fmt, va );
+		buf[n] = 0;
+		return n;
 	}
 #endif // _MSC_VER
 
-	// Make this class behave as if it's a buffer_t
+	// Make this class transparently behave as if it's a character array
 	inline operator buffer_t& () { return buf; }
 	inline operator const_buffer_t& () const { return buf; }
-	inline T& operator[] ( unsigned int i ) { assert(i>=0 && i<L); return buf[i]; }
-	inline const T& operator[] ( unsigned int i ) const { assert(i>=0 && i<L); return buf[i]; }
+	inline T& operator[] ( unsigned int i ) { assert(i<L); return buf[i]; }
+	inline const T& operator[] ( unsigned int i ) const { assert(i<L); return buf[i]; }
 };
+
+//------------------------------------------------
+// Function va_printf()
+//------------------------------------------------
+// Helper with convenient auto type deduction.
+template< unsigned long L, typename T >
+inline va_buf<L,T> va_printf( const T* fmt, ... )
+{
+	va_list va;
+	va_start( va, fmt );
+	va_buf<L,T> buf;
+	buf.printex( fmt, va );
+	return buf;
+}
 
 }
 
